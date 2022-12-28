266c5879a906432121b0f7d8a49636862f1de0e8
MamoudouDiallo-192151 <medmodiallo19@gmail.com>
2022-12-28 14:48:27 +0100

    création des entités Bien, Categorie, et Contact

---
+++ src/Entity/Bien.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BienRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BienRepository::class)
+ */
+class Bien
+{
+    const STATUSBIEN = [
+        0 => "En Location",
+        1 => "En Vente"
+    ];
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $codePostal;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $surface;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $statusBien;
+
+    //Cette colonne permet de supprimer tous les biens affectés  à une categorie qui vas être supprimer
+    #[ORM\JoinColumn(onDelete: "CASCADE")]
+    /**
+     * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="biens")
+     */
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?int
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(int $codePostal): self
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    public function getSurface(): ?float
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(float $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getStatusBien(): ?int
+    {
+        return $this->statusBien;
+    }
+
+    public function getStatusBienType(): ?String
+    {
+        return self::STATUSBIEN[$this->statusBien];
+    }
+
+    public function setStatusBien(int $statusBien): self
+    {
+        $this->statusBien = $statusBien;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
